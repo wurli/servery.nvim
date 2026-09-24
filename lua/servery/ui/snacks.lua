f19aea15cdb5394bef62b900e92095ce6ff8e2f1
Jacob Scott <jscott2718@gmail.com>
2026-09-24 11:53:24 +0100

    fix(snacks): filtering

--- lua/servery/ui/snacks.lua
+++ lua/servery/ui/snacks.lua
@@ -1,10 +1,16 @@
 ---@diagnostic disable: param-type-mismatch
 local M = {}
 
----@param items? servery.PickerItem[]
-M.select = function(items)
+M.select = function()
 	Snacks.picker.pick("servery_sessions", {
-		items = items or require("servery").get_picker_items(),
+		finder = function()
+			local new_items = require("servery").get_picker_items() --[[@as snacks.picker.finder.result]]
+			for i, item in ipairs(new_items) do
+				item.text = item:display_name()
+				item.idx = i
+			end
+			return new_items
+		end,
 		---@param item servery.PickerItem
 		format = function(item, _picker)
 			local icon = item:icon()
@@ -17,6 +23,7 @@ M.select = function(items)
 				{ item:time_since_active(), "ServeryTime" },
 			}
 		end,
+		sort = function(a, b) return a.idx < b.idx end,
 		layout = { preview = false },
 		win = {
 			input = {
@@ -28,14 +35,24 @@ M.select = function(items)
 			},
 		},
 		actions = {
+			---@param picker snacks.Picker
 			---@param item servery.PickerItem
-			switch = function(_picker, item, _action) item:switch() end,
+			switch = function(picker, item, _action)
+				item:switch()
+				picker:close()
+			end,
 			---@param item servery.PickerItem
 			switch_and_detach = function(_picker, item, _action) item:switch(true) end,
+			---@param picker snacks.Picker
 			---@param item servery.PickerItem
-			spawn = function(_picker, item, _action) item:spawn_new() end,
+			spawn = function(picker, item, _action)
+				item:spawn_new()
+				vim.defer_fn(function() picker:refresh() end, 500)
+			end,
 		},
 	})
 end
+
+M.select()
 
 return M
